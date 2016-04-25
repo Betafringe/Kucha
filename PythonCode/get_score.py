54788b1821d4372072204c26cccb37672c329911
Betafringe <micro_q@hotmail.com>
2016-04-25 21:21:23 +0800

    Added files via upload

---
+++ PythonCode/get_score.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+class Student(object):
+
+    def __init__(self, name, score):
+
+        self.__name = name
+
+        self.__score = score
+
+    def print_score(self):
+
+        print '%s: %s' % (self.__name, self.__score)
+
+    def get_name(self):
+
+        return self.__name
+
+    def get_score(self):
+
+        return self.__score
+
+bart = Student('Bart Simpson', 59)
+
+lisa = Student('Lisa Simpson', 87)
+
+print bart.get_score()
